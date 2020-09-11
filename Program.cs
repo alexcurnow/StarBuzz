af0022b10a6602ce1060764f3816315410d0e119
Alex Curnow <alexcurnow@gmail.com>
2020-09-11 12:12:30 -0500

    ...

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace Starbuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Beverage beverage = new Espresso();
+            Console.WriteLine($"{beverage.getDescription()} ${beverage.cost()}");
+
+            Beverage beverage2 = new DarkRoast();
+            beverage2 = new Mocha(beverage2);
+            beverage2 = new Mocha(beverage2);
+            beverage2 = new Whip(beverage2);
+            Console.WriteLine($"{beverage2.getDescription()} ${beverage2.cost()}");
+
         }
     }
 }
